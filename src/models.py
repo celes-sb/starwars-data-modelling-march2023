b25a51b534e760d3954fd0f443638d63950c95cb
Celeste Bareiro <cnyx7@proton.me>
2023-03-28 02:20:56 +0000

    first and last commit

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
+"""
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
@@ -28,6 +28,39 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+"""
+
+#class Gender(str, enum.Enum):
+ #   MALE = "male"
+  #  FEMALE = "female"
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(50), nullable=False)
+    pass
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    pass
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    pass
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('people.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    pass
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')

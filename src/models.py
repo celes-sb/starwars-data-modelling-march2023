edf68b339bad0501958c5c06c96e80e8aadbaaed
Celeste Bareiro <cnyx7@proton.me>
2023-03-30 21:30:46 +0000

    added more columns

--- src/models.py
+++ src/models.py
@@ -37,20 +37,40 @@ class Address(Base):
 class People(Base):
     __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    mass = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+    eye_color = Column(String(50), nullable=False)
+    birth_year = Column(String(50), nullable=False)
     gender = Column(String(50), nullable=False)
     pass
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    diameter = Column(String(50), primary_key=True)
+    rotation_period = Column(String(50), nullable=False)
+    orbital_period = Column(String(50), nullable=False)
+    gravity = Column(String(50), nullable=False)
+    population = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
+    terrain = Column(String(50), nullable=False)
+    surface_water = Column(String(50), nullable=False)
     pass
 
 class Vehicle(Base):
     __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(50), nullable=False)
+    manufacturer = Column(String(100), nullable=False)
+    cost_in_credits = Column(String(50), nullable=False)
+    length = Column(String(50), nullable=False)
+    crew = Column(String(50), nullable=False)
+    passengers = Column(String(50), nullable=False)
     pass
 
 class Favorite(Base):
